b3251c888d43d3be08d0d06b45f4904a46347807
barisbasar1209 <basar.baris1209@gmail.com>
2023-08-01 00:06:41 +0200

    removed comments

--- main.cpp
+++ main.cpp
@@ -3,33 +3,6 @@
 #include<iostream> 
 #include"Graph.h"
 
-// TODO: Test
-/*class Vertex{
-	public:
-		std::string name=""; 
-		int degree=-1; 
-		std::list<Vertex> neighbors; 
-		Vertex(std::string v_name) : name(v_name){degree=0;}   		
-		Vertex(std::string v_name, const std::list<Vertex>& v_neighbors) : name(v_name), neighbors(v_neighbors){degree = neighbors.size();}
-		//Vertex(const Vertex& v) name(v.name), degree(v.degree), neighbors(v.neighbors){}
-	void print_vertex(){
-		std::cout<<"name: "<<name<<std::endl<<"degree: "<<degree<<std::endl; 
-		if (!neighbors.empty()){
-			for (const Vertex& v : neighbors){
-				std::cout<<v.name<<", "; 
-			}	
-			std::cout<<std::endl; 
-		}
-	}
-}; 
-
-void copy_list(std::list<Vertex>& new_list, std::list<Vertex>& old_list){
-	for(Vertex& v : old_list){
-		new_list.push_back(v); 	
-	}
-}
-*/
-
 int main(){
 	Vertex v1("v1"); 
 	std::list<Vertex> lst; 
