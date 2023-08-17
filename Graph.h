ec84a49c9f9cdd825ab789f0702e630515ec77f7
barisbasar1209 <basar.baris1209@gmail.com>
2023-08-17 17:55:50 +0200

    included dynamic flag into vertex class

--- Graph.h
+++ Graph.h
@@ -6,45 +6,50 @@
 #include<algorithm>
 #include<cmath>
 
-// TODO: Test
+# define DYNAMIC true 
+
 class Vertex{
 	public:
+		// attributes
 		int name=-1; 
 		int degree=-1; 
+		bool is_dynamic=false; 
 		std::list<Vertex*> neighbors; 
-		Vertex(int v_name) : name(v_name){degree=0;}   		
+		// constructors
+		Vertex(int v_name) : name(v_name){degree=0;}
+		Vertex(int v_name, bool v_is_dynamic) : name(v_name), is_dynamic(v_is_dynamic){degree=0;}   		
 		Vertex(int v_name, const std::list<Vertex*>& v_neighbors) : name(v_name), neighbors(v_neighbors){degree = neighbors.size();}
-
-	void print_vertex(void){
-		std::cout<<"name: "<<name<<'\n'<<"degree: "<<degree<<'\n'; 
-		if (!neighbors.empty()){
-			for (Vertex *v : neighbors){
-				std::cout<<v->name<<", "; 
-			}	
-			std::cout<<'\n'; 
+		// methods 
+		void print_vertex(void){
+			std::cout<<"name: "<<name<<'\n'<<"degree: "<<degree<<'\n'; 
+			if (!neighbors.empty()){
+				for (Vertex *v : neighbors){
+					std::cout<<v->name<<", "; 
+				}	
+				std::cout<<'\n'; 
+			}
 		}
-	}
 }; 
 
 class Edge{
 	public: 
+		// attributes 
 		Vertex *first; 
 		Vertex *second; 
-		
+		// constructors	
 		Edge(Vertex *v1, Vertex *v2) : first(v1), second(v2){}
 		Edge(const Edge& e) : first(e.first), second(e.second){}
 
-	bool operator==(const Edge& e) const{
-		return (first==e.first && second==e.second) || (first==e.second && second==e.first); 	
-	}
-	void print_edge(void){
-		std::cout<<"Edge: {"<<first->name<<","<<second->name<<"}"; 
-		std::cout<<'\n'; 
-	}
-	bool isIncident(Vertex *v){
-		return first == v || second == v; 	
-	}
-	
+		bool operator==(const Edge& e) const{
+			return (first==e.first && second==e.second) || (first==e.second && second==e.first); 	
+		}
+		void print_edge(void){
+			std::cout<<"Edge: {"<<first->name<<","<<second->name<<"}"; 
+			std::cout<<'\n'; 
+		}
+		bool isIncident(Vertex *v){
+			return first == v || second == v; 	
+		}
 }; 
 
 class Graph{
@@ -65,7 +70,7 @@ class Graph{
 			n = adj_matrix.size(); 
 			// init V
 			for (int i=0; i<n; i++){
-				Vertex *v = new Vertex(i); // dynamic allocation with i as the name
+				Vertex *v = new Vertex(i, DYNAMIC); // dynamic allocation with i as the name
 				V.push_back(v); 
 			}
 			// init E
@@ -85,55 +90,52 @@ class Graph{
 				}
 			}
 		}
-	// methods 
-	void add_edge(Edge& e){
-		auto iter = std::find(E.begin(), E.end(), e); 
-		if (iter == E.end()){
-			E.push_back(e); 
-			e.first->degree++; 
-			e.second->degree++; 
-			e.first->neighbors.push_back(e.second);
-			e.second->neighbors.push_back(e.first);
-			m++; 
-		}	
-	}
-	void delete_edge(Edge& e){
-		Vertex *v1(e.first); 
-		Vertex *v2(e.second); 
-		v1->degree--; 
-		v2->degree--; 
-		v1->neighbors.remove(v2); 
-		v2->neighbors.remove(v1); 
-		E.remove(e); 	
-	}
-
-	void delete_vertex(Vertex *v){
-		V.remove(v); 
-		for (Edge& e : E){
-			if (e.isIncident(v)){
-				delete_edge(e); 	
+		// methods 
+		void add_edge(Edge& e){
+			auto iter = std::find(E.begin(), E.end(), e); 
+			if (iter == E.end()){
+				E.push_back(e); 
+				e.first->degree++; 
+				e.second->degree++; 
+				e.first->neighbors.push_back(e.second);
+				e.second->neighbors.push_back(e.first);
+				m++; 
 			}	
 		}
-		for (Vertex *u : v->neighbors){
-			u->neighbors.remove(v); 
-			u->degree--; 	
+		void delete_edge(Edge& e){
+			Vertex *v1(e.first); 
+			Vertex *v2(e.second); 
+			v1->degree--; 
+			v2->degree--; 
+			v1->neighbors.remove(v2); 
+			v2->neighbors.remove(v1); 
+			E.remove(e); 	
 		}
-	}
-	void add_edge_set(std::list<Edge> E_G){
-		for (Edge& e : E_G) add_edge(e); 	
-	}
+		void delete_vertex(Vertex *v){
+			V.remove(v); 
+			for (Edge& e : E){
+				if (e.isIncident(v)) delete_edge(e);	
+			}
+			for (Vertex *u : v->neighbors){
+				u->neighbors.remove(v); 
+				u->degree--; 	
+			}
+		}
+		void add_edge_set(std::list<Edge> E_G){
+			for (Edge& e : E_G) add_edge(e); 	
+		}
 
-	void print_graph(void){
-		std::cout<<"n = "<<n<<'\n'<<"m = "<<m<<'\n'; 
-		if (!V.empty()){
-			for (Vertex *v : V) std::cout<<v->name<<" "; 
+		void print_graph(void){
+			std::cout<<"n = "<<n<<'\n'<<"m = "<<m<<'\n'; 
+			if (!V.empty()){
+				for (Vertex *v : V) std::cout<<v->name<<" "; 
+			}
+			std::cout<<'\n'; 
+			if (!E.empty()){
+				for (Edge& e : E) std::cout<<"{"<<e.first->name<<" "<<e.second->name<<"} "; 
+			}
+			std::cout<<'\n'; 
 		}
-		std::cout<<'\n'; 
-		if (!E.empty()){
-			for (Edge& e : E) std::cout<<"{"<<e.first->name<<" "<<e.second->name<<"} "; 
-		}
-		std::cout<<'\n'; 
-	}
 }; 
 
 void copy_list(std::list<Vertex*>& new_list, std::list<Vertex*>& old_list){
